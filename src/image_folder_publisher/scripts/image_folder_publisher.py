74146ef03ea5aadad9f457ef97cc0bd14cb7b26c
amc-nu <abrahammonrroy@yahoo.com>
2019-12-07 17:22:21 +0900

    image_folder_publisher. add loop parameter, add timestamp
    Signed-off-by: amc-nu <abrahammonrroy@yahoo.com>

--- src/image_folder_publisher/scripts/image_folder_publisher.py
+++ src/image_folder_publisher/scripts/image_folder_publisher.py
@@ -16,7 +16,6 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class image_folder_publisher:
-
     def __init__(self):
         self.__app_name = "image_folder_publisher"
 
@@ -36,6 +35,9 @@ class image_folder_publisher:
         self._frame_id = rospy.get_param('~frame_id', 'camera')
         rospy.loginfo("[%s] (frame_id) Frame ID set to  %s", self.__app_name, self._frame_id)
 
+        self._loop = rospy.get_param('~loop', 1)
+        rospy.loginfo("[%s] (loop) Loop  %d time(s) (set it -1 for infinite)", self.__app_name, self._loop)
+
         self._image_folder = rospy.get_param('~image_folder', '')
         if self._image_folder == '' or not os.path.exists(self._image_folder) or not os.path.isdir(self._image_folder):
             rospy.logfatal("[%s] (image_folder) Invalid Image folder", self.__app_name)
@@ -49,19 +51,23 @@ class image_folder_publisher:
         if self._sort_files:
             files_in_dir.sort()
         try:
-            for f in files_in_dir:
-                if not rospy.is_shutdown():
-                    if isfile(join(self._image_folder, f)):
-                        cv_image = cv2.imread(join(self._image_folder, f))
-                        if cv_image is not None:
-                            ros_msg = self._cv_bridge.cv2_to_imgmsg(cv_image, "bgr8")
-                            ros_msg.header.frame_id = self._frame_id
-                            self._image_publisher.publish(ros_msg)
-                        else:
-                            rospy.loginfo("[%s] Invalid image file %s", self.__app_name, join(self._image_folder, f))
-                        ros_rate.sleep()
-                else:
-                    return
+            while self._loop != 0:
+                for f in files_in_dir:
+                    if not rospy.is_shutdown():
+                        if isfile(join(self._image_folder, f)):
+                            cv_image = cv2.imread(join(self._image_folder, f))
+                            if cv_image is not None:
+                                ros_msg = self._cv_bridge.cv2_to_imgmsg(cv_image, "bgr8")
+                                ros_msg.header.frame_id = self._frame_id
+                                ros_msg.header.stamp = rospy.Time.now()
+                                self._image_publisher.publish(ros_msg)
+                                rospy.loginfo("[%s] Published %s", self.__app_name, join(self._image_folder, f))
+                            else:
+                                rospy.loginfo("[%s] Invalid image file %s", self.__app_name, join(self._image_folder, f))
+                            ros_rate.sleep()
+                    else:
+                        return
+                self._loop = self._loop - 1
         except CvBridgeError as e:
             rospy.logerr(e)
 
